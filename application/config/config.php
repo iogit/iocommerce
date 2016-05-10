f153e59eb3641db3d7fdfccab10407ef6ab4004a
oraklius <oraklius@gmail.com>
2016-05-09 21:56:37 -0500

    Base URL updated -> Azure webapp URL added

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Codeigniter_3.0/ecommerce/';
+$config['base_url'] = 'http://iocommerce.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
